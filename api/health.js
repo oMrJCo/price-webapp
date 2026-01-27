40e493d5c72311cda69d9927e846ba451465eefd
oMrJCo <athiwatjc@gmail.com>
2026-01-28 01:44:27 +0700

    reset

--- api/health.js
+++
@@ -1,9 +0,0 @@
-// /api/health.js
-export default function handler(req, res) {
-  res.status(200).json({
-    ok: true,
-    hasClientId: !!process.env.GITHUB_CLIENT_ID,
-    hasClientSecret: !!process.env.GITHUB_CLIENT_SECRET,
-    hasRedirect: !!process.env.OAUTH_REDIRECT_URI,
-  });
-}

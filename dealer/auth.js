3baf424fe4d8dcceca8a26bb225c5297a4f56d41
oMrJCo <athiwatjc@gmail.com>
2026-02-18 22:24:51 +0700

    de

---
+++ dealer/auth.js
@@ -0,0 +1,21 @@
+
+function checkAuth() {
+  const isAuth = localStorage.getItem("dealerAuth");
+  if (isAuth !== "true") {
+    window.location.href = "/dealer/index.html";
+  }
+}
+
+function login(password) {
+  if (password === DEALER_PASSWORD) {
+    localStorage.setItem("dealerAuth", "true");
+    window.location.href = "/dealer/home.html";
+  } else {
+    alert("รหัสไม่ถูกต้อง");
+  }
+}
+
+function logout() {
+  localStorage.removeItem("dealerAuth");
+  window.location.href = "/dealer/index.html";
+}
